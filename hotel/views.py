570cce72b81a98dba3e45cd94835b4dcf79412c3
Aithal <rachanaaithal@rachanaaithal88@gmail.com>
2019-03-18 00:44:15 +0530

    basic booking flow complete V2.1(with userid)

--- hotel/views.py
+++ hotel/views.py
@@ -17,3 +17,15 @@ def hotel_book(request, id, category):
     print('book_page',id, category)
     context={'id':id, 'category': category}
     return render(request, 'confirm_booking.html', context)
+
+@login_required
+def booking_confirmed(request, hotel_id, category, transaction_id):
+    print('booked_page:', hotel_id, category, transaction_id)
+    context={'hotel_id':hotel_id, 'category': category, 'transaction_id': transaction_id}
+    return render(request, 'booking_confirmed.html', context)
+
+@login_required
+def booking_cancelled(request, hotel_id, category):
+    print('booked_page:', hotel_id, category)
+    context={'hotel_id':hotel_id, 'category': category}
+    return render(request, 'booking_cancelled.html', context)
