b11362b4abdd60dae6b1d206009329bc5a7b11dd
Aithal <rachanaaithal@rachanaaithal88@gmail.com>
2019-04-24 13:06:21 +0530

    Allow all hosts + search cleared before appending again

--- hotel/static/js/search.js
+++ hotel/static/js/search.js
@@ -22,7 +22,7 @@ window.onload = function () {
 			success: function(data){
 				console.log(data);
 				result=data;
-				//$('#results').html('');
+				$('#results').html('');
 
 				if(data['response'].length>0){
 					$('#noresults').hide();
