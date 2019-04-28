4ce0817f6c7f50954a9576e8c230c55a96a34a76
YeshaswiniMukunda <43567860+YeshaswiniMukunda@users.noreply.github.com>
2019-04-28 10:31:44 +0530

    hotel operator

--- customer/views.py
+++ customer/views.py
@@ -13,3 +13,17 @@ def user_register(request):
 @login_required
 def user_profile(request):
 	return render(request,'profile.html')
+	
+def register(request):
+	return render(request,'new_register.html')
+	
+def oper_register(request):
+	return render(request,'oper_register.html')
+	
+
+from django.shortcuts import redirect	
+def verify(request):
+	if request.user.is_superuser:
+		return render(request,'verify.html')
+	else:
+		return redirect('/hotel/')
